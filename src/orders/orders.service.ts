952860dc94087e42247ef62b30bf6820eef9b89a
Luan A. <barkon32@tuta.io>
2023-12-09 00:51:29 -0300

    changed to transaction mode in transaction execution

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -23,69 +23,71 @@ export class OrdersService {
   }
 
   async executeTransaction(input: InputExecuteTransactionDto) {
-    const order = await this.prismaService.order.findUniqueOrThrow({
-      where: { id: input.order_id },
-    });
-    await this.prismaService.order.update({
-      where: { id: input.order_id },
-      data: {
-        partial: order.partial - input.negotiated_shares,
-        status: input.status,
-        Transactions: {
-          create: {
-            related_investor_id: input.related_investor_id,
-            broker_transaction_id: input.broker_investor_id,
-            shares: input.negotiated_shares,
+    this.prismaService.$transaction(async (prisma) => {
+      const order = await prisma.order.findUniqueOrThrow({
+        where: { id: input.order_id },
+      });
+      await prisma.order.update({
+        where: { id: input.order_id },
+        data: {
+          partial: order.partial - input.negotiated_shares,
+          status: input.status,
+          Transactions: {
+            create: {
+              related_investor_id: input.related_investor_id,
+              broker_transaction_id: input.broker_investor_id,
+              shares: input.negotiated_shares,
+              price: input.price,
+            },
+          },
+        },
+      });
+      if (input.status == OrderStatus.CLOSED) {
+        await prisma.asset.update({
+          where: { id: order.asset_id },
+          data: {
             price: input.price,
           },
-        },
-      },
-    });
-    if (input.status == OrderStatus.CLOSED) {
-      await this.prismaService.asset.update({
-        where: { id: order.asset_id },
-        data: {
-          price: input.price,
-        },
-      });
-      await this.prismaService.walletAsset.findUnique({
-        where: {
-          wallet_id_asset_id: {
-            asset_id: order.asset_id,
-            wallet_id: order.wallet_id,
-          },
-        },
-      });
-      const walletAsset = await this.prismaService.walletAsset.findUnique({
-        where: {
-          wallet_id_asset_id: {
-            asset_id: order.asset_id,
-            wallet_id: order.wallet_id,
-          },
-        },
-      });
-      if (walletAsset) {
-        await this.prismaService.walletAsset.update({
+        });
+        await prisma.walletAsset.findUnique({
           where: {
             wallet_id_asset_id: {
               asset_id: order.asset_id,
               wallet_id: order.wallet_id,
             },
           },
-          data: {
-            shares: walletAsset.shares + input.negotiated_shares,
+        });
+        const walletAsset = await prisma.walletAsset.findUnique({
+          where: {
+            wallet_id_asset_id: {
+              asset_id: order.asset_id,
+              wallet_id: order.wallet_id,
+            },
           },
         });
-      } else {
-        await this.prismaService.walletAsset.create({
-          data: {
-            asset_id: order.asset_id,
-            wallet_id: order.wallet_id,
-            shares: input.negotiated_shares,
-            version: 1,
-          },
-        });
+        if (walletAsset) {
+          await prisma.walletAsset.update({
+            where: {
+              wallet_id_asset_id: {
+                asset_id: order.asset_id,
+                wallet_id: order.wallet_id,
+              },
+            },
+            data: {
+              shares: walletAsset.shares + input.negotiated_shares,
+            },
+          });
+        } else {
+          await prisma.walletAsset.create({
+            data: {
+              asset_id: order.asset_id,
+              wallet_id: order.wallet_id,
+              shares: input.negotiated_shares,
+              version: 1,
+            },
+          });
+        }
       }
-    }
+    });
   }
 }
